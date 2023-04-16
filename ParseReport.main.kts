bc5514bd2d49df8ee0795706c235bde28a20dde9
sebastian@lehrbaum.de <slehrbaum@laptop0958.fritz.box>
2023-04-16 20:04:41 +0200

    Add export file

--- ParseReport.main.kts
+++ ParseReport.main.kts
@@ -4,19 +4,17 @@ import kotlin.time.Duration
 import kotlin.time.Duration.Companion.hours
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
-import kotlinx.datetime.LocalTime
 import org.jetbrains.kotlinx.dataframe.DataFrame
-import org.jetbrains.kotlinx.dataframe.api.add
 import org.jetbrains.kotlinx.dataframe.api.column
 import org.jetbrains.kotlinx.dataframe.api.convert
 import org.jetbrains.kotlinx.dataframe.api.dropLast
 import org.jetbrains.kotlinx.dataframe.api.groupBy
 import org.jetbrains.kotlinx.dataframe.api.into
 import org.jetbrains.kotlinx.dataframe.api.rename
-import org.jetbrains.kotlinx.dataframe.api.sum
+import org.jetbrains.kotlinx.dataframe.api.reorderColumnsByName
 import org.jetbrains.kotlinx.dataframe.api.sumOf
-import org.jetbrains.kotlinx.dataframe.api.with
 import org.jetbrains.kotlinx.dataframe.io.read
+import org.jetbrains.kotlinx.dataframe.io.writeCSV
 
 val DurationRaw by column<String>("Duration")
 val Comment by column<String>()
@@ -27,7 +25,9 @@ val data = DataFrame.read(args[0])[DurationRaw, Comment]
     .sumOf { it[DurationRaw].parseHHMM().inWholeSeconds }
     .convert("sum") { (it as Long).seconds }
     .rename("sum").into("Duration")
+    .reorderColumnsByName(desc = true)
 println(data)
+data.writeCSV("output.csv")
 
 fun String.parseHHMM(): Duration {
     val (hh, mm) = split(':').map(String::toInt)
