a5b3129ee608391069c016449bc2fb17650b767b
sebastian@lehrbaum.de <slehrbaum@laptop0958.fritz.box>
2023-04-16 20:09:02 +0200

    Remove Type safety, didn't provide much

--- ParseReport.main.kts
+++ ParseReport.main.kts
@@ -5,24 +5,21 @@ import kotlin.time.Duration.Companion.hours
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 import org.jetbrains.kotlinx.dataframe.DataFrame
-import org.jetbrains.kotlinx.dataframe.api.column
 import org.jetbrains.kotlinx.dataframe.api.convert
 import org.jetbrains.kotlinx.dataframe.api.dropLast
 import org.jetbrains.kotlinx.dataframe.api.groupBy
 import org.jetbrains.kotlinx.dataframe.api.into
 import org.jetbrains.kotlinx.dataframe.api.rename
 import org.jetbrains.kotlinx.dataframe.api.reorderColumnsByName
+import org.jetbrains.kotlinx.dataframe.api.select
 import org.jetbrains.kotlinx.dataframe.api.sumOf
 import org.jetbrains.kotlinx.dataframe.io.read
 import org.jetbrains.kotlinx.dataframe.io.writeCSV
 
-val DurationRaw by column<String>("Duration")
-val Comment by column<String>()
-
-val data = DataFrame.read(args[0])[DurationRaw, Comment]
+val data = DataFrame.read(args[0]).select("Duration", "Comment")
     .dropLast(4)
-    .groupBy(Comment)
-    .sumOf { it[DurationRaw].parseHHMM().inWholeSeconds }
+    .groupBy("Comment")
+    .sumOf { (it["Duration"] as String).parseHHMM().inWholeSeconds }
     .convert("sum") { (it as Long).seconds }
     .rename("sum").into("Duration")
     .reorderColumnsByName(desc = true)
